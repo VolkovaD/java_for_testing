1c829b9eaa9cdaa2c28cfb3c3af7c1517767d89e
VolkovaD <volkova.darya89@gmail.com>
2018-02-07 22:22:33 +0600

    Пример с созданием функции из Лекции 1.7

--- sandbox/src/main/java/kz/qa/jft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/kz/qa/jft/sandbox/FirstProgram.java
@@ -1,8 +1,24 @@
 package kz.qa.jft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.sql.SQLOutput;
+
 public class FirstProgram {
 
 	public static void main(String[] args){
-		System.out.println("Hello, world");
+		hello("World");
+        hello("Dasha");
+
+        double l = 8;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 	}
+
+	public static void hello(String somebody){
+	    System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l){
+	    return l*l;
+    }
 }
