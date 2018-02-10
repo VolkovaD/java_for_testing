1410e50299c0e54fb4c4dd8df077ed02efd4b1fb
VolkovaD <volkova.darya89@gmail.com>
2018-02-10 21:00:20 +0600

    Добавление метода и исправление функции по поиску расстояния

--- sandbox/src/main/java/kz/qa/jft/point/Point.java
+++ sandbox/src/main/java/kz/qa/jft/point/Point.java
@@ -4,5 +4,14 @@ public class Point {
     double x;
     double y;
 
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
 
+    public double distanceMethod(Point p){
+        double diffX = (this.x - p.x);
+        double diffY = (this.y - p.y);
+        return Math.sqrt(Math.pow(diffX,2) + (Math.pow(diffY, 2)));
+    }
 }
--- sandbox/src/main/java/kz/qa/jft/point/mainRunClass.java
+++ sandbox/src/main/java/kz/qa/jft/point/mainRunClass.java
@@ -1,22 +1,20 @@
 package kz.qa.jft.point;
 
-public class mainRunClass {
+public class MainRunClass {
+
     public static void main(String[] args){
-        Point p1 = new Point();
-        Point p2 = new Point();
+        Point p1 = new Point(6, 6);
+        Point p2 = new Point(9, 9);
 
-        p1.x = 1;
-        p1.y = 1;
-
-        p2.x = 11;
-        p2.y = 11;
-
-
-        System.out.println("Расстояние между двумя точками p1( " + p1.x + "," + p1.y + ") и р2( "  + p2.x + "," +  p2.y + ") = " + distance(p1, p2));
-
+        System.out.println("Через функцию distanceFunction():\nРасстояние между двумя точками p1(" + p1.x + "," + p1.y +
+                ") и р2(" + p2.x + "," +  p2.y + ") = " + distanceFunction(p1, p2));
+        System.out.println("Через метод distanceMethod():\nРасстояние между двумя точками p1(" + p1.x + "," + p1.y +
+                ") и р2(" + p2.x + "," +  p2.y + ") = " + p2.distanceMethod(p1));
     }
 
-    public static double distance (Point p1, Point p2){
-        return Math.sqrt(Math.pow((p2.x - p1.x),2) + (Math.pow((p2.y - p1.y), 2)));
+    public static double distanceFunction(Point p1, Point p2){
+        double diffX = (p2.x - p1.x);
+        double diffY = (p2.y - p1.y);
+        return Math.sqrt(Math.pow(diffX,2) + (Math.pow(diffY, 2)));
     }
 }
