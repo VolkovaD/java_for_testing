59d4893998fd7225b05dac5c39812aafcf60ba80
VolkovaD <volkova.darya89@gmail.com>
2018-02-22 15:07:32 +0600

    Выделила специализированные классы-помощники

---
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,23 @@
+package kz.qa.jft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SessionHelper {
+    private FirefoxDriver wd;
+
+    public SessionHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void login(String username, String pass) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(pass);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+}
