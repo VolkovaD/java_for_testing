1380132eb194189ee01d88378c184b97745c42be
VolkovaD <volkova.darya89@gmail.com>
2018-02-28 21:49:03 +0600

    Добавила тест для редактирования контакта

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/appmanager/ContactHelper.java
@@ -42,4 +42,12 @@ public class ContactHelper extends BaseHelper{
     public void acceptAlert() {
         wd.switchTo().alert().accept();
     }
+
+    public void initContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
 }
---
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/ContactModificationTest.java
@@ -0,0 +1,17 @@
+package kz.qa.jft.addressbook.tests;
+
+import kz.qa.jft.addressbook.model.ContactData;
+import org.testng.annotations.Test;
+
+public class ContactModificationTest extends TestBase {
+
+    @Test
+    public void testContactModification(){
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("Irina", "Savina", "Ira","",
+                "+77770333322", "test@gmail.com", "1995"));
+        app.getContactHelper().submitContactModification();
+        app.getContactHelper().returnToHomePage();
+    }
+}
