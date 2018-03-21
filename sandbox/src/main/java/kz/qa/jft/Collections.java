74b061ff6babf0338478f35ed6eeec5a86edea38
VolkovaD <volkova.darya89@gmail.com>
2018-03-21 16:18:16 +0600

    Изучаем коллекции

---
+++ sandbox/src/main/java/kz/qa/jft/Collections.java
@@ -0,0 +1,25 @@
+package kz.qa.jft;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+    public static void main(String[] args){
+        String[] langs = {"Java", "C#", "Python", "PHP"};
+
+      //  List<String> languages = new ArrayList<String>();
+
+/*        languages.add("Java");
+        languages.add("C#");
+        languages.add("Python");
+        languages.add("PHP");*/
+
+        List<String>  languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+        for (String l : languages){
+            System.out.println("Я хочу выучить "+ l);
+        }
+
+    }
+}
