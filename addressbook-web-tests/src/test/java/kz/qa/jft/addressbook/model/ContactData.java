eb0b4a7a5f64fcf46dde8bdb76a62736509529e7
VolkovaD <volkova.darya89@gmail.com>
2018-03-26 16:44:46 +0600

    Сравнение списков при модификации контактов

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/model/ContactData.java
@@ -1,7 +1,7 @@
 package kz.qa.jft.addressbook.model;
 
 public class ContactData {
-
+    private final String id;
     private final String firstname;
     private final String lastname;
     private final String nickname;
@@ -11,7 +11,8 @@ public class ContactData {
     private final String bYear;
     private String group;
 
-    public ContactData(String firstname, String lastname, String nickname, String address, String mobile, String email, String bYear, String group) {
+    public ContactData(String id, String lastname, String firstname, String nickname, String address, String mobile, String email, String bYear, String group) {
+        this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
         this.nickname = nickname;
@@ -20,6 +21,22 @@ public class ContactData {
         this.email = email;
         this.bYear = bYear;
         this.group = group;
+    }
+
+    public ContactData(String lastname, String firstname, String nickname, String address, String mobile, String email, String bYear, String group) {
+        this.id = null;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+        this.bYear = bYear;
+        this.group = group;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getFirstname() {
@@ -55,29 +72,31 @@ public class ContactData {
     }
 
     @Override
-    public String toString() {
-        return "ContactData{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         ContactData that = (ContactData) o;
 
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
         if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
         return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
     }
 
     @Override
     public int hashCode() {
-        int result = firstname != null ? firstname.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
         result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
         return result;
     }
 
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id='" + id + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/ContactModificationTest.java
@@ -4,6 +4,7 @@ import kz.qa.jft.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTest extends TestBase {
@@ -13,17 +14,23 @@ public class ContactModificationTest extends TestBase {
         app.getNavigationHelper().gotoHomePage();
         if(! app.getContactHelper().isThereAContact()){
             app.getNavigationHelper().gotoContactPage();
-            app.getContactHelper().createContact(new ContactData("Inna", "Ivanova", "Inna", "Kazakhstan",
+            app.getContactHelper().createContact(new ContactData("Ivanova", "Inna","Inna", "Kazakhstan",
                     "+77770000022", "test@gmail.com", "1995", "test1"), true);
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().initContactModification(before.size() - 1);
-        app.getContactHelper().fillContactForm(new ContactData("Irina", "Savina", "Ira","",
-                "+77770333322", "test@gmail.com", "1995", null), false);
+        int index = before.size() - 1;
+        app.getContactHelper().initContactModification(index);
+        ContactData contact = new ContactData(before.get(index).getId(),"Savina", "Irina","Ira", "",
+                "+77770333322", "test@gmail.com", "1995", null);
+        app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(index);
+        before.add(contact);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
 }
