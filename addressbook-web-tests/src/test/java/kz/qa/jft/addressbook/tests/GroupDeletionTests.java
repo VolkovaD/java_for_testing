af267669205484f6bd1d7f325e518038202f10ce
VolkovaD <volkova.darya89@gmail.com>
2018-03-27 18:07:01 +0600

    Вынесли в предусловие подготовку к запуску теста

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/GroupDeletionTests.java
@@ -2,27 +2,32 @@ package kz.qa.jft.addressbook.tests;
 
 import kz.qa.jft.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-    @Test
-    public void testGroupDeletion() {
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().gotoGroupPage();
         if(! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1",null,null));
-
         }
+    }
+
+    @Test
+    public void testGroupDeletion() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
+        int index = before.size() - 1;
+        app.getGroupHelper().selectGroup(index);
         app.getGroupHelper().deleteSelectedGroup();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() - 1);
+        Assert.assertEquals(after.size(), index);
 
-        before.remove(before.size() - 1);
+        before.remove(index);
             Assert.assertEquals(before, after);
     }
 }
