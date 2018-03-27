2b346193b3aaa864728fd33bf7b1bbeaa4b8bb16
VolkovaD <volkova.darya89@gmail.com>
2018-03-27 17:45:38 +0600

    Оптимизировали запуск браузера

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/TestBase.java
@@ -1,8 +1,8 @@
 package kz.qa.jft.addressbook.tests;
 
 import kz.qa.jft.addressbook.appmanager.ApplicationManager;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import sun.plugin2.util.BrowserType;
 
 import static org.openqa.selenium.remote.BrowserType.CHROME;
@@ -14,14 +14,14 @@ import static org.openqa.selenium.remote.BrowserType.IE;
  */
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(CHROME);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
