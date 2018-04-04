adb7fabf95c6a8e461bc7df283c4d704a7ea6a96
VolkovaD <volkova.darya89@gmail.com>
2018-04-05 00:35:41 +0600

    Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/generators/ContactDataGenerator.java
@@ -53,19 +53,18 @@ public class ContactDataGenerator {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
-
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         contacts.getClass();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
     private List<ContactData> generateContacts(int count) {
