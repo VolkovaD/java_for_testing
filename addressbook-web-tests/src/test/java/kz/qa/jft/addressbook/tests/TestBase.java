358e6d4da7f4dfc6542f73dff31522f19516a5df
VolkovaD <volkova.darya89@gmail.com>
2018-04-07 14:19:55 +0600

    Добавили библиотеку для логирования и написали пример с логированием

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/tests/TestBase.java
@@ -2,13 +2,22 @@ package kz.qa.jft.addressbook.tests;
 
 import kz.qa.jft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Содержит общие функции и методы для всех тестов
  */
 public class TestBase {
+
+    Logger logger = (Logger) LoggerFactory.getLogger(ContactCreationTest.class);
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -20,5 +29,15 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test " + m.getName() + "wiht params" + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.debug("Stop test " + m.getName());
     }
 }
