206b9c3ef518075862b86f7bbfc270b0fd41ddf5
VolkovaD <volkova.darya89@gmail.com>
2018-04-09 00:49:09 +0600

    Правки в генераторе контактов.

--- addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/kz/qa/jft/addressbook/generators/ContactDataGenerator.java
@@ -77,7 +77,7 @@ public class ContactDataGenerator {
                     .withHomePhone(String.format("Home tel %s", i))
                     .withMobile(String.format("Mobile tel %s", i))
                     .withEmail(String.format("Email %s", i))
-                    .withbYear(String.format("BYear %s", i))
+                    .withbYear(String.format("1987"))
                     .withGroup("test1"));
         }
         return contacts;
