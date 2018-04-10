b5a8c4bb4ce8002446980da4945868f465cfb9ba
VolkovaD <volkova.darya89@gmail.com>
2018-04-10 09:36:48 +0600

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/kz/qa/jft/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package kz.qa.jft.mantis.tests;
+
+import kz.qa.jft.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Содержит общие функции и методы для всех тестов
+ */
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        app.stop();
+    }
+}
